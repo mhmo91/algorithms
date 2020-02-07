c59a2f0b6b97a9533aad7a73e82042e42cd97986
Muhammad Hasan <muhammad@dartssolutions.com>
2020-02-07 18:19:08 +0200

    minor refactoring

--- time-conversion.py
+++ time-conversion.py
@@ -1,9 +1,14 @@
 # problem: https://www.hackerrank.com/challenges/time-conversion/problem
-time = '07:05:45PM'
-hours = time[0:2]
-if time.endswith('AM'):
-    if int(hours) == 12:
-        time = time.replace(hours, '00', 1)
-elif int(hours) < 12:
-    time = time.replace(hours, str(int(hours)+12), 1)
-print(time[0:len(time)-2])
+
+
+def convertToMilitaryTime(time):
+    hours = time[0:2]
+    if time.endswith('AM'):
+        if int(hours) == 12:
+            time = time.replace(hours, '00', 1)
+    elif int(hours) < 12:
+        time = time.replace(hours, str(int(hours)+12), 1)
+    return(time[0:len(time)-2])
+
+
+print(convertToMilitaryTime('07:05:45PM'))  # test function

ab307ad6338edcb057c10e8acb675634e03f040b
Muhammad Hasan <muhammad@dartssolutions.com>
2020-02-24 13:35:15 +0200

    rewrite to the algorithm num of carriers

--- num-of-carriers-in-additions.py
+++ num-of-carriers-in-additions.py
@@ -1,27 +1,21 @@
 # i'm an empty file that Mo uses to play!
 def numberOfCarryOperations(x, y):
-    lenOfBigNum = 0
-    if x >= y:
-        lenOfBigNum = len(str(x))
-    else:
-        lenOfBigNum = len(str(y))
+    result = 0
     x = str(x)
     y = str(y)
-    result = 0
-    for i in range(lenOfBigNum, 0, -1):
-        firstNum = int(x[len(x)-1])
-        secNum = int(y[len(y)-1])
-        reminder = 0
-        print(firstNum, secNum)
-        if firstNum is not None and secNum is not None:
-            reminder = (firstNum + secNum) - 9
-            if reminder >= 1:
-                result += 1
-                x = str(int(x) + reminder)
-        else:
-            return result
-
+    reminder = 0
+    while len(x) > 0 or len(y) > 0:
+        a, b = (0, 0)
+        if len(x) > 0:
+            a = int(x[-1])
+            x = x[:-1]
+        if len(y) > 0:
+            b = int(y[-1])
+            y = y[:-1]
+        reminder = (a + b + reminder)-9
+        if(reminder > 0):
+            result += 1
     return result
 
 
-print(numberOfCarryOperations(900, 11))
+print(numberOfCarryOperations(798, 2))
